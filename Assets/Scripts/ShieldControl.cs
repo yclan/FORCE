5ccd99ad01b4b283247e2ef4cb36db186dbbef71
Bruce Lan <Bruce@darkwindmedia.com>
2015-01-11 18:28:34 -0500

    Added Touch Input & model of the new circle fortress

---
+++ Assets/Scripts/ShieldControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldControl : TouchLogic {
+
+	public float speed = 10f;
+	private Vector3 finger;
+	private Transform myTrans, camTrans;
+	
+	void Start ()
+	{
+		myTrans = this.transform;
+		camTrans = Camera.main.transform;
+	}
+	
+	//separated to own function so it can be called from multiple functions
+	void LookAtFinger()
+	{
+		//Debug.Log ("Ray hits the shield!");
+		//myTrans.Translate (Vector3.forward * speed * Time.deltaTime);
+		//z of ScreenToWorldPoint is distance from camera into the world, so we need to find this object's distance from the camera to make it stay on the same plane
+		Vector3 tempTouch = new Vector3(Input.GetTouch(touch2Watch).position.x, Input.GetTouch(touch2Watch).position.y, 
+		                                camTrans.position.z - myTrans.position.z);
+		//Convert screen to world space
+		finger = Camera.main.ScreenToWorldPoint(tempTouch);
+		
+		//look at finger position
+		myTrans.LookAt(finger, Vector3.back);
+		//this.transform.parent.transform.LookAt(finger, Vector3.forward);
+	}
+	public void OnTouchMoved3D()
+	{
+		LookAtFinger();
+	}
+	public void OnTouchStayed3D()
+	{
+		LookAtFinger();
+	}
+	public void OnTouchBegan3D()
+	{
+		touch2Watch = TouchLogic.currTouch;
+	}
+}
---
+++ Assets/Scripts/TouchLogic.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchLogic : MonoBehaviour 
+{
+	public static int currTouch = 0;//so other scripts can know what touch is currently on screen
+	private Ray ray;//this will be the ray that we cast from our touch into the scene
+	private RaycastHit rayHitInfo = new RaycastHit();//return the info of the object that was hit by the ray
+	[HideInInspector]
+	public int touch2Watch = 64;
+	
+	void Update () 
+	{
+		//is there a touch on screen?
+		if(Input.touches.Length <= 0)
+		{
+			//if no touches then execute this code
+		}
+		else //if there is a touch
+		{
+			//loop through all the the touches on screen
+			for(int i = 0; i < Input.touchCount; i++)
+			{
+				currTouch = i;
+				//Debug.Log(currTouch);
+
+				ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);//creates ray from screen point position
+				switch(Input.GetTouch(i).phase)
+				{
+				case TouchPhase.Began:
+					//OnTouchBeganAnywhere();
+					//this.SendMessage("OnTouchBeganAnyWhere");
+					if(Physics.Raycast(ray, out rayHitInfo))
+						rayHitInfo.transform.gameObject.SendMessage("OnTouchBegan3D");
+						//Debug.Log (rayHitInfo.transform.gameObject);
+					break;
+				case TouchPhase.Ended:
+					//OnTouchEndedAnywhere();
+					//this.SendMessage("OnTouchEndedAnywhere");
+					if(Physics.Raycast(ray, out rayHitInfo))
+						rayHitInfo.transform.gameObject.SendMessage("OnTouchEnded3D");
+						//Debug.Log (rayHitInfo.transform.gameObject);
+					break;
+				case TouchPhase.Moved:
+					//OnTouchMovedAnywhere();
+					//this.SendMessage("OnTouchMovedAnywhere");
+					if(Physics.Raycast(ray, out rayHitInfo))
+						rayHitInfo.transform.gameObject.SendMessage("OnTouchMoved3D");
+						//Debug.Log (rayHitInfo.transform.gameObject);
+					break;
+				case TouchPhase.Stationary:
+					//OnTouchStayedAnywhere();
+					//this.SendMessage("OnTouchStayedAnywhere");
+					if(Physics.Raycast(ray, out rayHitInfo))
+						rayHitInfo.transform.gameObject.SendMessage("OnTouchStayed3D");
+						//Debug.Log (rayHitInfo.transform.gameObject);
+					break;
+				}
+			}
+		}
+	}
+}
